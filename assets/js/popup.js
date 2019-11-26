b521b3c6d891164999603c3f351f0c8ce20f2126
chesminsky <chesminsky@gmail.com>
2019-11-26 23:13:32 +0300

    vimeo

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,13 +1,36 @@
 window.onload = function () {
+    // call
     var popup = document.getElementById("popup-call-container");
     var buttonOpen = document.getElementById("button-open");
     var buttonClose = document.getElementById("popup-call-button-close");
 
     buttonOpen.onclick = function ( ) {
-        popup.classList.add("popup-call-container-active");
+        popup.classList.add("is-active");
     }
 
     buttonClose.onclick = function () {
-        popup.classList.remove("popup-call-container-active");
+        popup.classList.remove("is-active");
+    }
+
+    // video
+    var popupVideoOpen = document.getElementById("popup-vimeo-open");
+    var popupVideoClose = document.getElementById("popup-vimeo-close");
+    var popupVideo = document.getElementById("popup-vimeo");
+    var iframe = document.querySelector('#popup-vimeo iframe');
+    var player = new Vimeo.Player(iframe);
+
+    popupVideoOpen.onclick = function ( ) {
+        popupVideo.classList.add("is-active");
+        player.play();
+    }
+
+    popupVideo.onclick = function () {
+        popupVideo.classList.remove("is-active");
+        player.pause();
+    }
+
+    popupVideoClose.onclick = function () {
+        popupVideo.classList.remove("is-active");
+        player.pause();
     }
 }
